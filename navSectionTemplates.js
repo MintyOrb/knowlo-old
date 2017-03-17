a15b3f28dae82ab48a7b52781968c4ec24a4de9f
Tim Borny <bornytm@gmail.com>
2017-03-18 01:07:06 +0700

    add template file

---
+++ navSectionTemplates.js
@@ -0,0 +1,28 @@
+
+const home = {
+  template: `<div>hello world! home here.</div>`
+}
+
+const status = {
+  template: `<div>hello world! status here.</div>`
+}
+
+const about = {
+  template: `<div>hello world! about here.</div>`
+}
+
+const principals = {
+  template: `<div>hello world! principals here.</div>`
+}
+
+const profile = {
+  template: `<div>hello world! profile here.</div>`
+}
+
+const involved = {
+  template: `<div>hello world! involved here.</div>`
+}
+
+const legal = {
+  template: `<div>hello world! legal here.</div>`
+}
